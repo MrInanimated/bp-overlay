355b34be45a06d261789816b98c1d72b4071f7e1
Tianlin Zhang <tianlinzhang@gmail.com>
2015-05-03 19:28:09 +0100

    Fixed merge conflicts and updated minified version

--- dist/bpoverlay.user.js
+++ dist/bpoverlay.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BombParty Overlay
-// @version      1.5.4
+// @version      1.5.5
 // @description  Overlay + Utilities for BombParty!
 // @icon         https://raw.githubusercontent.com/MrInanimated/bp-overlay/master/dist/icon.png
 // @icon64       https://raw.githubusercontent.com/MrInanimated/bp-overlay/master/dist/icon64.png
