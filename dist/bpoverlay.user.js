2017610e8a8a060ba234482affdebf5300116f29
MrInanimated <tianlinzhang@gmail.com>
2016-04-13 01:58:30 +0100

    version bump

--- dist/bpoverlay.user.js
+++ dist/bpoverlay.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BombParty Overlay
-// @version      1.7.2
+// @version      1.7.3
 // @description  Overlay + Utilities for BombParty!
 // @icon         https://raw.githubusercontent.com/MrInanimated/bp-overlay/master/dist/icon.png
 // @icon64       https://raw.githubusercontent.com/MrInanimated/bp-overlay/master/dist/icon64.png
